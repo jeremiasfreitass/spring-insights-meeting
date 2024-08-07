3b07a08fbbe12938aadfaba3e44f287b26d33577
Jeremias Freitas <jeremias.freitas@dtidigital.com.br>
2024-08-07 10:01:50 -0300

    ajuste

--- src/main/java/com/example/insights_meeting/Configuration/WebClientConfig.java
+++ src/main/java/com/example/insights_meeting/Configuration/WebClientConfig.java
@@ -3,10 +3,15 @@ package com.example.insights_meeting.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.Logger;
+import reactor.util.Loggers;
 
 @Configuration
 public class WebClientConfig {
+    private static final Logger log = Loggers.getLogger(WebClientConfig.class);
 
     @Value("${openai.api.key}")
     private String openaiApiKey;
@@ -19,6 +24,7 @@ public class WebClientConfig {
         return WebClient.builder()
                 .baseUrl(openaiBaseUrl)
                 .defaultHeader("Authorization", "Bearer " + openaiApiKey)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
